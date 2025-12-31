a6894d6a527f766f56a8037eedc8d5033580a535
seufster71 <seufster@gmail.com>
2025-12-31 10:33:34 -0500

    update all

--- dashboard/dashboard-container.js
+++ dashboard/dashboard-container.js
@@ -5,22 +5,33 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import * as actions from './dashboard-actions';
+import fuLogger from '../../../core/common/fu-logger';
 import DashboardView from "../../../memberView/colony/dashboard/dashboard-view";
+import BaseContainer from '../../../core/container/base-container';
 
-function CCDashboardContainer() {
-	const dashboardState = useSelector((state) => state.dashboard);
+function CCDashboardContainer({location,navigate}) {
+	const itemState = useSelector((state) => state.dashboard);
+	const session = useSelector((state) => state.session);
 	const appPrefs = useSelector((state) => state.appPrefs);
 	const dispatch = useDispatch();
 	
 	useEffect(() => {
-    	dispatch(actions.getDashboard())
+    	dispatch(actions.init({lang:session.selected.lang}));
   	}, [])
 	
-	if (dashboardState != null) {
+	
+	
+	
+	
+	
+	
+	fuLogger.log({level:'TRACE',loc:'ECMarketContainer::render',msg:"Hi there"});
+	if (itemState.items != null) {
 		return (
 			<DashboardView
 			itemState={dashboardState}
 			appPrefs={appPrefs}
+			session={session}
 			/>
 		);
 	} else {
--- dashboard/dashboard-reducer.js
+++ dashboard/dashboard-reducer.js
@@ -1,17 +1,53 @@
-/**
- * 
+/*
+ * Copyright (C) 2020 The ToastHub Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
- export default function dashboardReducer(state = {}, action) {
-	let myState = {};
+ import reducerUtils from '../../../core/common/reducer-utils';
+ 
+ export default function memberCCDASHBOARDReducer(state = {}, action) {
 	switch(action.type) {
+		
+		case 'CC_DASHBOARD_INIT': {
+    		if (action.responseJson != null && action.responseJson.params != null) {
+	    	    return Object.assign({}, state, {
+	    	      	prefTexts: Object.assign({}, state.prefTexts, reducerUtils.getPrefTexts(action)),
+					prefLabels: Object.assign({}, state.prefLabels, reducerUtils.getPrefLabels(action)),
+					prefOptions: Object.assign({}, state.prefOptions, reducerUtils.getPrefOptions(action)),
+					columns: reducerUtils.getColumns(action),
+					itemCount: reducerUtils.getItemCount(action),
+					items: reducerUtils.getItems(action),
+					listLimit: reducerUtils.getListLimit(action),
+					listStart: reducerUtils.getListStart(action),
+					orderCriteria: [{'orderColumn':'CC_DASHBOARD_TABLE_TITLE','orderDir':'ASC'}],
+					searchCriteria: [{'searchValue':'','searchColumn':'CC_DASHBOARD_TABLE_TITLE'}],
+					paginationSegment: 1,
+					selected: null,
+					inputFields:null,
+					view: "MAIN",
+					pageName:"CC_DASHBOARD",
+					isDeleteModalOpen: false,
+					errors:null, 
+					warns:null, 
+					successes:null,
+					searchValue:""
+	    	    });
+    	  	} else {
+    	    	return state;
+    	  	}
+    	}
 		case 'CC_DASHBOARD_INPUT_CHANGE': {
-			if (action.params != null) {
-				let clone = Object.assign({}, state);
-				clone.test_field = action.params.value;
-				return clone;
-			} else {
-        		return state;
-    		}
+			return reducerUtils.updateInputChange(state,action);
     	}
 		case 'CC_DASHBOARD_GET': {
 			if (action.responseJson != null && action.responseJson.params != null) {
